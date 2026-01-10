ddcb820715cd36f596699cafc2e78fb851a8ce61
Daniil Oberlev <denioberlev@gmail.com>
2026-01-11 01:28:36 +0300

    feat(reexport): add reexport file for ui

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,30 @@
+export { Badge } from './Badge';
+export { Button } from './Button';
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from './Card';
+export {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './Dialog';
+export { EmptyState } from './EmptyState';
+export { Input } from './Input';
+export { Label } from './Label';
+export {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './Select';
+export { Tabs, TabsContent, TabsList, TabsTrigger } from './Tabs';
+export { Textarea } from './Textarea';
